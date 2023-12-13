983ff6c9bbda2100b676c53bc519f0baaa811114
Đăng Khoa Dương Trần <126379959+DuongTranDangKhoa@users.noreply.github.com>
2023-12-13 20:16:57 +0700

    Update DBcontext

--- Assgn_PRJ301_ShoesStore/src/java/dal/DBContext.java
+++ Assgn_PRJ301_ShoesStore/src/java/dal/DBContext.java
@@ -14,7 +14,7 @@ public class DBContext {
         try {
             String user = "sa";
             String pass = "12345";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=ShoesStore";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=FlowerShop";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
